711caef76188a52b764b8454d795fa1aff338867
boss1104 <alphaBoss1104@gmail.com>
2022-02-18 15:28:45 -0800

    testing commerce.js api key

--- client/src/lib/commerce.js
+++ client/src/lib/commerce.js
@@ -1,6 +1,6 @@
 import Commerce from '@chec/commerce.js';
 
 export const commerce = new Commerce(
-	process.env.REACT_APP_CHEC_PUBLIC_KEY,
+	'pk_test_389008981d9dafec47b055af9effda6600f8d1a0063fa',
 	true
 );
